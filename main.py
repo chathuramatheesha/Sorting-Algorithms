f5a9264e3b5bd9c681ce983c6b6e769a5ab10e46
Chathura Matheesha <chathuramatheesha@gmail.com>
2023-04-23 06:21:58 +0530

    Added Quick Sort

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-# This is a sample Python script.
+from sorting_algorithms.quick_sort import quick_sort_partition
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+my_list = [26, 12, 23, 10, 24, 27, 29]
+quick_sort_partition(my_list, 0, len(my_list)-1)
---
+++ sorting_algorithms/quick_sort.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+# Quick Sort with List Comprehensions
+def quick_sort(sorting_list):
+    if len(sorting_list) <= 1:
+        return sorting_list
+    else:
+        pivot = sorting_list[0]
+        left = [x for x in sorting_list[1:] if x < pivot]
+        right = [x for x in sorting_list[1:] if x >= pivot]
+        return quick_sort(left) + [pivot] + quick_sort(right)
+
+
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i + 1
+
+
+def quick_sort_partition(array, low, high) -> List:
+    if low < high:
+        pi = partition(array, low, high)
+        quick_sort_partition(array, low, pi - 1)
+        quick_sort_partition(array, pi + 1, high)
+    else:
+        return array
